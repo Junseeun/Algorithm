a5b49737e70ca21a31be2c859dd7c3bfb5c6dc4a
Junseeun <165360452+Junseeun@users.noreply.github.com>
2024-05-24 00:24:59 +0900

    Update 괄호.cc

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -11,31 +11,29 @@ int main() {
     
     cin >> T;
     for (int i = 0; i < T; i++) {
-        stack<char> s;    // i가 달라질때마다 스택은 초기화되어야함
-        result = "YES";    // result 값은 YES로 초기화
+        stack<char> s;    
+        result = "YES";    
 
         cin >> sentence;
         sentence_size = sentence.size();
 
-        // 문자열의 처음부터 탐색 시작
+        
         for (int j = 0; j < sentence_size; j++) {
-            if (sentence[j] == '(') {  // 여는 괄호일때
+            if (sentence[j] == '(') {  
                 s.push('(');
             }
 
-            else if (sentence[j] == ')') {  // 닫는 괄호일때,
-                if (!s.empty()) {  // 스택이 비어있지 않으면,
+            else if (sentence[j] == ')') {  
+                if (!s.empty()) { 
                     s.pop();
                 }
-                else {  // 비어있으면
+                else {  
                     result = "NO";
                 }
             }
         }
 
-        // 마지막까지 연산완료 후,
-        // 여는 괄호와 닫는 괄호의 짝이 맞지 않는 경우
-        // 즉, 스택이 비어있지 않은 경우
+        
         if (!s.empty()) {
             result = "NO";
         }
@@ -46,3 +44,52 @@ int main() {
 
     return 0;
 }
+
+
+/*
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    
+    string sentence; // 입력될 문자열
+    string result; // 입력된 문자열
+    int flag = 0;
+    
+    int T;
+    cin >> T;
+    
+    for(int i=0; i<T; i++){
+        stack<char> s;
+        result = "YES";
+        
+        cin >> sentence;
+        flag=sentence.size();
+        
+        for(int j=0;j<flag; j++){
+            if(sentence[j] == '(')
+            {
+                s.push('(');
+            }
+            else if(sentence[j] == ')'){
+                if(!s.empty()){
+                    s.pop();
+                }
+                else{
+                    result = "No";
+                }
+            }
+        }
+        if(!s.empty()){
+            result = "No";
+        
+    }
+        cout<<result<<"\n";
+    
+    }
+    return 0;
+}
+*/
+// 아래코드는 실행시켰을 때 출력이되는데 백준에 제출했을때 틀렸다고 나온다. 왜 그런지 모르겠다. 
